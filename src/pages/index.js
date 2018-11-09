3b2c378d94b3fdd06a1b9e92e02eda42179e688a
mrhut10 <mrhut10@protonmail.com>
2018-11-10 02:44:15 +1100

    easter egg speaks to you

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Speak } from 'libreact/lib/Speak';
 
 import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout showHero>
     <h1>Welcome</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Speak text="welcome to futures Fine Furniture & Bedding" />
     <Link to="/delivery/">Delivered To Your Door</Link>
   </Layout>
 )
