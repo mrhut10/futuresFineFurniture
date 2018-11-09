993f19786b89962632d479b6c031288d8a375db1
mrhut10 <mrhut10@protonmail.com>
2018-11-09 13:38:26 +1100

    hero Image working

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,15 +1,46 @@
 import React from 'react'
 import logo from '../images/slimLogo.svg'
 import hero from '../images/heroLogo.svg'
-//import Stocked from './stokedText'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
+const HeroBackgroundImage = () => (
+  <StaticQuery
+    query={graphql`
+      query { 
+        placeholderImage: file(relativePath: {eq: "DINING_ALASKA.jpg"}){
+          childImageSharp {
+            fluid(maxWidth: 960){
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={
+      data => <div style={{ backgroundColor: 'white' }}>
+        <Img alt="Image of Dinning Table" style={{ opacity: '0.8' }} fluid={data.placeholderImage.childImageSharp.fluid} />
+      </div>
+    }
+  />
+)
 
 const Logo = ({ showHero }) => (
-  <div style={{ margin: '0 auto', textAlign: 'center' }}>
+  <div style={{ margin: '0 auto', textAlign: 'center', position: "relative" }}>
+    {
+      showHero
+        ? <HeroBackgroundImage style={{ position: 'absolute', }} />
+        : null
+    }
     <img
       style={{
         marginBottom: '-0.2em',
         fontSize: ".1em",
-
+        position: showHero ? 'absolute' : 'inherit',
+        top: '0px',
+        left: showHero ? '50%' : 'inherit',
+        transform: showHero ? 'translateX(-50%)' : 'inherit',
+        marginTop: '10px',
       }}
       src={showHero ? hero : logo} alt="Futures Fine Furniture & Bedding"
     />
