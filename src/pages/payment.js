a6b15e7b800b44ac9b88b98b9717faafd93347dc
mrhut10 <mrhut10@protonmail.com>
2018-11-11 02:52:01 +1100

    payment page

---
+++ src/pages/payment.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const PaymentPage = () => (
+  <Layout>
+    <h1>Invovative Payment Options</h1>
+    <p>
+      Helpful and Flexible payment options to help you get your Quality Furniture quicker
+    </p>
+    <h3>Standard Payments Methods ( Instore & Online )</h3>
+    <ul>
+      <li>Cards (Visa, master, Amex and most others)</li>
+      <li>Cash</li>
+    </ul>
+    <h3>Flexible Payment Options ( Instore )</h3>
+    <ul>
+      <li>Layby's ( over max 3 months, with a non-refundable 20% deposit )</li>
+      <li>Further Flexiable Finance Options Comming</li>
+    </ul>
+  </Layout>
+)
+
+export default PaymentPage
