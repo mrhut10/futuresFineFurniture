fb402b67070c0b0b1147878cbe7e68b571a4a95e
mrhut10 <mrhut10@protonmail.com>
2018-11-23 13:36:46 +1100

    fixed snipcart apikey

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,7 +31,7 @@ const Layout = ({ children, showHero }) => (
         >
           <html lang="en" />
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-          <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" id="snipcart" data-api-key={data.site.snipCartApiKey}></script>
+          <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" id="snipcart" data-api-key={data.site.site.Metadata.snipCartApiKey}></script>
           <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" type="text/css" rel="stylesheet" />
 
         </Helmet>
