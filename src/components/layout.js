5e1d28d0927e02ef11019870d24772db29dddaa9
mrhut10 <mrhut10@protonmail.com>
2019-03-08 10:28:20 +1100

    remove redirect

--- src/components/layout.js
+++ src/components/layout.js
@@ -37,7 +37,6 @@ const Layout = ({ children, showHero, crumbs }) => (
           ]}
         >
           <html lang="en" />
-          <script>location.href="https://www.facebook.com/futuresfinefurniture/"</script>
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
           <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" id="snipcart" data-api-key={"YzUxMzk5NTItOThmOC00NzM3LWE5NmUtNGViMGVmNDNhNTdkNjM2NzY2ODMyMDY2NzQzNzg2"}></script>
           <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" type="text/css" rel="stylesheet" />
