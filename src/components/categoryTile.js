aea36e0e0898c3ca3c28ee370044196a5e90f505
mrhut10 <mrhut10@protonmail.com>
2019-03-11 04:05:35 +1100

    simplified categoryTile

--- src/components/categoryTile.js
+++ src/components/categoryTile.js
@@ -2,30 +2,11 @@
 import React from "react"
 import { Link } from 'gatsby'
 import { Card, Elevation } from "@blueprintjs/core";
-import { StaticQuery, graphql } from "gatsby"
 import CommingSoon from "./CommingSoon";
-const { compose, last, lift, pathOr, split, } = require('ramda')
 
 const ImageComponent = input => input ? <img alt="product" src={input} /> : null
 
 const CategoryTitle = ({ name, hoverText, slug, images, commingSoon }) => (
-  <StaticQuery 
-    query={graphql`{
-      allFile(filter: {sourceInstanceName: {eq: "contentImages"}}) {
-        edges {
-          node {
-            relativePath
-            childImageSharp {
-              fixed(width: 200) {
-                src
-              }
-            }
-          }
-        }
-      }
-    }
-    `}
-    render={data=>(
       <Link 
           to={slug || "/"} 
           title={hoverText}
@@ -47,39 +28,6 @@ const CategoryTitle = ({ name, hoverText, slug, images, commingSoon }) => (
           <br/>... more
         </Card>
       </Link>
-    )}
-  />
-)
-
-//      <CategoryTitle name={name} Img={Img} hoverText={hoverText} slug={slug} />
-
-const oldCategoryTitle = ({ name, Img, hoverText, slug }) => (
-  <Link
-    to={slug || "/"}
-    title={hoverText}
-    style={{
-      display: 'block',
-      position: 'relative',
-      width: '150px',
-      height: '150px',
-      margin: '5px',
-      textDecoration: 'none',
-      overflow: 'hidden',
-      border: '1px solid blue'
-    }}
-  >
-    <h4 style={{
-      position: 'absolute',
-      width: '100%',
-      top: 'calc(100px - 1em)',
-      textAlign: 'center'
-    }}>
-      {name}
-    </h4>
-    <div style={{ width: '200px', height: '200px' }}>
-
-    </div>
-  </Link>
 )
 
 export default CategoryTitle
