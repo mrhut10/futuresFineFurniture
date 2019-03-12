43f07164eda92029c2250ea58c360923855130e2
mrhut10 <mrhut10@protonmail.com>
2019-03-13 03:10:36 +1100

    adding offline functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 }
