a559c0f382db6c8639a2fa886d637eecbe1dfad6
mrhut10 <mrhut10@protonmail.com>
2019-03-15 12:23:12 +1100

    added from price within category page

--- src/components/categoryTile.js
+++ src/components/categoryTile.js
@@ -6,26 +6,31 @@ import CommingSoon from "./CommingSoon";
 
 const ImageComponent = (input,maxheight='350px') => input ? <img style={{maxheight:maxheight}} alt="product" src={input} /> : null
 
-const CategoryTitle = ({Children, name, hoverText, slug, images, commingSoon, height=250, width=250 }) => (
+const CategoryTitle = ({Children, name, hoverText, slug, images, commingSoon, height=350, width=250 }) => (
       <Link 
           to={slug || "/"} 
           title={hoverText}
           style={{}}
         >
         <Card 
-          style={{ padding: '15px', margin:'10px', height:`${height}px`, width:`${width}px`}}
+          style={{ padding: '15px', margin:'10px', width:`${width}px`}}
           interactive={true}
         elevation={Elevation.TWO}
         >
-          <h5>{name}</h5>
-          {
-            commingSoon
-            ?<CommingSoon/>
-            : images
-              ? ImageComponent(images,`calc(${height}-30px)`)
-              : ''
-          }
-          {Children}
+          <div style={{display:'grid',gridTemplate:`"1fr" "1fr" "1fr"`
+          }}>
+            <h5>{name}</h5>
+            <div>
+            {
+              commingSoon
+              ?<CommingSoon/>
+              : images
+                ? ImageComponent(images,`calc(${height}-30px)`)
+                : ''
+              }
+            </div>
+            <div>{Children}</div>
+          </div>
         </Card>
       </Link>
 )
