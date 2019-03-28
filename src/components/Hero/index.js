59f78d08c7dc65b11fea2556a37db4c6acae9956
mrhut10 <mrhut10@protonmail.com>
2019-03-29 10:28:45 +1100

    making slimHero display pages by default

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import { graphql, Link, StaticQuery } from 'gatsby';
 import Image from './Image';
-import Logo from '../../images/heroLogo.svg';
+import HeroImageBig from '../../images/heroLogo.svg';
+import HeroImageSlim from '../../images/slimLogo.svg';
 
-const Hero = () => (
+
+const HeroBig = ({data}) => (
+  <div className="bg-white flex justify-center max-w-xl relative w-full">
+        <div className="image-container opacity-75">
+            <Image />
+        </div>
+        <div className="max-w-md mx-auto pb-32 py-4 px-4 relative text-center w-full">
+          <Link to="/">
+            <img
+              className="w-auto"
+              src={HeroImageBig}
+              alt={data.site.siteMetadata.title}
+            />
+          </Link>
+        </div>
+      </div>
+)
+
+
+const HeroSlim = ({data}) => (
+  /* insert some of Lukes Magic tailwind css stuff below */
+  <div>
+    <Link to="/">
+      <img
+        className="w-auto"
+        src={HeroImageSlim}
+        alt={data.site.siteMetadata.title}
+      />
+    </Link>
+  </div>
+)
+
+
+const Hero = ({showHero=false}) => (
   <StaticQuery
     query={graphql`
       query HeroQuery {
@@ -14,22 +48,11 @@ const Hero = () => (
         }
       }
     `}
-    render={data => (
-      <div className="bg-white flex justify-center max-w-xl relative w-full">
-        <div className="image-container opacity-75">
-          <Image />
-        </div>
-        <div className="max-w-md mx-auto pb-32 py-4 px-4 relative text-center w-full">
-          <Link to="/">
-            <img
-              className="w-auto"
-              src={Logo}
-              alt={data.site.siteMetadata.title}
-            />
-          </Link>
-        </div>
-      </div>
-    )}
+    render={
+      data => showHero
+        ? <HeroBig data={data}/>
+        : <HeroSlim data={data}/>
+    }
   />
 );
 
