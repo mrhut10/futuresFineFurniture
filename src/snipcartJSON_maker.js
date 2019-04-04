b84c5e520082b4b3c691367d82811a02d852d5d6
mrhut10 <mrhut10@protonmail.com>
2019-04-05 05:24:27 +1100

    snipcartjson maker module

---
+++ src/snipcartJSON_maker.js
@@ -0,0 +1,46 @@
+const R = require('ramda')
+
+const spy = (input)=> {
+  console.log(input)
+  return input
+}
+
+exports.snipcart_json = R.compose(
+  R.map(R.compose(
+    spy,
+    (input) => {
+      return {
+        name: R.prop('title')(input),
+        id: R.prop('title')(input),
+        price:{
+          AUD:R.compose(
+            R.divide(R.__,100),
+            R.path(['variants',0,'price']
+          ))(input),
+        },
+        url:'/snipcartJSON',
+        customFields:R.compose(
+          R.ifElse(
+            R.compose(R.gt(R.__,1),R.length),
+            (allvarients) => {
+              return [
+                {
+                  name:"Option",
+                  options:allvarients.map(vari=>`${vari.varientName}[${positive((vari.price-R.pathOr(0,['0','price'])(allvarients))/100)}]`).join('|'),
+                  type:'dropdown'
+                }
+              ]
+            },
+            R.always([])
+          ),
+          R.prop('variants')
+        )(input),
+      }
+    },
+    R.path(['node','frontmatter'])
+  )),
+  R.path(['data','allMarkdownRemark','edges']),
+)
+
+const positive = (input) => `${input>=0?'+':''}${input}`
+
