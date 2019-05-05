bc80c6baee1d9d8f0c6874a6e29d9584ffd12a0d
Luke Bennett <luke.bennett88@gmail.com>
2019-05-05 21:13:49 +1000

    Formatting

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,54 +4,49 @@ import Image from './Image';
 import HeroImageBig from '../../images/heroLogo.svg';
 import HeroImageSlim from '../../images/slimLogo.svg';
 
-
-const HeroBig = ({data}) => (
+const HeroBig = ({ data }) => (
   <div
     className="bg-white flex justify-center max-w-xl relative"
-    style={{height:'23rem'}}
+    style={{ height: '23rem' }}
   >
-        {/*hero image */}
-        <div
-          className="image-container flex absolute flex opacity-75"
-        >
-            <Image />
-        </div>
-        {/* overlay */}
-        <div
-          className="max-w-md m-auto absolute w-full object-center"
-          style={{
-            justifyContent:'center',
-            alignItems:'center',
-            textAlign:'center',
-            margin:'0px auto',
-            top:'50%',
-            left:'50%',
-            transform: 'translate(-50%, -50%)'
-          }}
-        >
-            <img
-              className="w-auto"
-              src={HeroImageBig}
-              alt={data.site.siteMetadata.title}
-            />
-        </div>
-      </div>
-)
-
-
-const HeroSlim = ({data}) => (
-  /* insert some of Lukes Magic tailwind css stuff below */
-  <div>
+    {/* hero image */}
+    <div className="image-container flex absolute flex opacity-75">
+      <Image />
+    </div>
+    {/* overlay */}
+    <div
+      className="max-w-md m-auto absolute w-full object-center"
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        margin: '0px auto',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
       <img
         className="w-auto"
-        src={HeroImageSlim}
+        src={HeroImageBig}
         alt={data.site.siteMetadata.title}
       />
+    </div>
   </div>
-)
+);
 
+const HeroSlim = ({ data }) => (
+  /* insert some of Luke's Magic Tailwind CSS stuff below */
+  <div>
+    <img
+      className="w-auto"
+      src={HeroImageSlim}
+      alt={data.site.siteMetadata.title}
+    />
+  </div>
+);
 
-const Hero = ({showHero=false}) => (
+const Hero = ({ showHero = false }) => (
   <StaticQuery
     query={graphql`
       query HeroQuery {
@@ -62,10 +57,8 @@ const Hero = ({showHero=false}) => (
         }
       }
     `}
-    render={
-      data => showHero
-        ? <HeroBig data={data}/>
-        : <HeroSlim data={data}/>
+    render={data =>
+      showHero ? <HeroBig data={data} /> : <HeroSlim data={data} />
     }
   />
 );
