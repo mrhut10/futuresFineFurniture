3c243c723526f111c813a1560fe75c6fa3a8193e
Luke Bennett <luke.bennett88@gmail.com>
2019-05-05 21:11:50 +1000

    Formatting

--- src/components/categoryTile.js
+++ src/components/categoryTile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { Card, Elevation } from '@blueprintjs/core';
+// import { Card, Elevation } from '@blueprintjs/core';
 import ComingSoon from './ComingSoon';
 
 const ImageComponent = (input, maxheight = '350px') =>
@@ -17,31 +17,31 @@ const CategoryTitle = ({
   width = 250,
 }) => (
   <div className="flex flex-col p-2 md:w-1/3">
-    <div
-      className="flex flex-1 flex-col border no-underline p-4 rounded-lg shadow hover:shadow-lg"
-    >
+    <div className="flex flex-1 flex-col border no-underline p-4 rounded-lg shadow hover:shadow-lg">
+      <Link to={slug || '/'} title={hoverText}>
+        <h4 className="mb-4 text-blue-dark text-center text-xl">
+          {name} &rarr;
+        </h4>
+      </Link>
+      <div className="mb-auto">
+        {comingSoon ? (
+          <ComingSoon />
+        ) : images ? (
+          <Link to={slug || '/'} title={hoverText}>
+            {ImageComponent(images, `calc(${height}-30px)`)}
+          </Link>
+        ) : (
+          ''
+        )}
+      </div>
+      <div>{Children}</div>
       <Link
+        style={{ width: '100%', textAlign: 'right' }}
         to={slug || '/'}
         title={hoverText}
       >
-        <h4 className="mb-4 text-blue-dark text-center text-xl">{name} &rarr;</h4>
+        see more ...
       </Link>
-        <div className="mb-auto">
-          {comingSoon ? (
-            <ComingSoon />
-          ) : images ? (
-            <Link
-              to={slug || '/'}
-              title={hoverText}
-            >
-              {ImageComponent(images, `calc(${height}-30px)`)}
-            </Link>
-          ) : (
-            ''
-          )}
-        </div>
-      <div>{Children}</div>
-      <Link style={{width:'100%',textAlign:'right'}} to={slug||'/'} title={hoverText}>see more ...</Link>
     </div>
   </div>
 );
