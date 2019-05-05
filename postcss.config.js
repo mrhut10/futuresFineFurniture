b01b3a76c97cd4be869e59edbb0bb9ff311cd204
Luke Bennett <luke.bennett88@gmail.com>
2019-05-05 21:01:32 +1000

    Move require to top of file

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
+const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
 
 module.exports = {
-  plugins: [tailwindcss('./tailwind.config.js'), require('autoprefixer')],
+  plugins: [tailwindcss('./tailwind.config.js'), autoprefixer],
 };
