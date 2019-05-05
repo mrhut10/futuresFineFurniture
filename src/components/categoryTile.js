49662a6fc5ea64daa5666155ff4fec96d05dfd84
Luke Bennett <luke.bennett88@gmail.com>
2019-05-05 23:24:57 +1000

    Style product cards

--- src/components/categoryTile.js
+++ src/components/categoryTile.js
@@ -17,31 +17,22 @@ const CategoryTitle = ({
   width = 250,
 }) => (
   <div className="flex flex-col p-2 md:w-1/3">
-    <div className="flex flex-1 flex-col border no-underline p-4 rounded-lg shadow hover:shadow-lg">
+    <div className="flex flex-1 flex-col border no-underline overflow-hidden rounded-lg shadow hover:shadow-lg">
       <Link to={slug || '/'} title={hoverText}>
-        <h4 className="mb-4 text-blue-dark text-center text-xl">
-          {name} &rarr;
+        <h4 className="mb-0 p-4 text-center text-grey-darkest text-sm tracking-wide uppercase">
+          {name}
         </h4>
       </Link>
-      <div className="mb-auto">
-        {comingSoon ? (
-          <ComingSoon />
-        ) : images ? (
-          <Link to={slug || '/'} title={hoverText}>
-            {ImageComponent(images, `calc(${height}-30px)`)}
-          </Link>
-        ) : (
-          ''
-        )}
-      </div>
+      {comingSoon ? (
+        <ComingSoon />
+      ) : images ? (
+        <Link className="mb-auto p-4" to={slug || '/'} title={hoverText}>
+          {ImageComponent(images, `calc(${height}-30px)`)}
+        </Link>
+      ) : (
+        ''
+      )}
       <div>{Children}</div>
-      <Link
-        style={{ width: '100%', textAlign: 'right' }}
-        to={slug || '/'}
-        title={hoverText}
-      >
-        see more ...
-      </Link>
     </div>
   </div>
 );
