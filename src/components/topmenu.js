bf57c4042375cfc4fa90092aa450273e01a7cd86
Luke Bennett <luke.bennett88@gmail.com>
2019-05-05 21:12:59 +1000

    Formatting

--- src/components/topmenu.js
+++ src/components/topmenu.js
@@ -5,62 +5,57 @@ import { IconNames } from '@blueprintjs/icons';
 import { ICON } from '@blueprintjs/core/lib/esm/common/classes';
 
 const content = {
-  left:[
+  left: [
     {
-      text:' Home',
-      link:'/',
-      icon:IconNames.HOME
+      text: ' Home',
+      link: '/',
+      icon: IconNames.HOME,
     },
     {
-      text:' Categories',
-      link:'/range',
-      icon:IconNames.SHOP
+      text: ' Categories',
+      link: '/range',
+      icon: IconNames.SHOP,
     },
     {
-      text:' Contact',
-      link:'/contact',
-      icon:IconNames.FOLDER_CLOSE
+      text: ' Contact',
+      link: '/contact',
+      icon: IconNames.FOLDER_CLOSE,
     },
   ],
-  right:[
+  right: [
     {
-      text:' Cart',
-      link:'#',
-      icon:IconNames.SHOPPING_CART
+      text: ' Cart',
+      link: '#',
+      icon: IconNames.SHOPPING_CART,
     },
-  ]
-}
+  ],
+};
 const SizeSafeMenu = () => <NewMenu />;
 
 const NewMenu = () => (
   <Navbar className="max-w-xl mx-auto w-full">
-    <NavbarGroup className="bg-grey-darkest flex p-4"
-      align={Alignment.LEFT}
-    >
-      {
-        content.left.map(item=>(
-          <Link
-            to={item.link}
-            className="mr-4 no-underline hover:underline text-grey-light hover:text-yellow-lighter"
-            key={item.link}
-          >
-            <Button
-              large
-              className="fill-current mr-2 text-grey-light"
-              icon={item.icon}
-              text={item.text}/>
-          </Link>
-        ))
-      }      
+    <NavbarGroup className="bg-grey-darkest flex p-4" align={Alignment.LEFT}>
+      {content.left.map(item => (
+        <Link
+          to={item.link}
+          className="mr-4 no-underline hover:underline text-grey-light hover:text-yellow-lighter"
+          key={item.link}
+        >
+          <Button
+            large
+            className="fill-current mr-2 text-grey-light"
+            icon={item.icon}
+            text={item.text}
+          />
+        </Link>
+      ))}
       <Button
         large
         className="fill-current mr-2 text-grey-light snipcart-checkout"
         icon={IconNames.SHOPPING_CART}
         text=" Shopping Cart"
-      >
-      </Button>
+      />
     </NavbarGroup>
-    
   </Navbar>
 );
 
