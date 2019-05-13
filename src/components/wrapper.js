356505070a9998689ac9c47ec4fdc15ef969cf18
mrhut10 <mrhut10@protonmail.com>
2019-05-14 08:52:21 +1000

    lint - error - wrapper

--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Wrapper = ({ children }) => (
   <div className="max-w-lg mx-auto p-4">{children}</div>
 );
+Wrapper.propTypes = {
+  children: PropTypes.any,
+};
 
 export default Wrapper;
