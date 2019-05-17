a70efbb28a9c5357fa3127ff948e8b2e210f9516
mrhut10 <mrhut10@protonmail.com>
2019-05-17 10:51:57 +1000

    added mailchimp popup

--- src/components/layout.js
+++ src/components/layout.js
@@ -38,6 +38,9 @@ const Layout = ({ children, showHero }) => (
         >
           <html lang="en" />
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js" />
+          <script id="mcjs">
+            {`!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/4ef8471ffe4887c7437198f2e/fa1103d389ef79306e0e4a1f7.js");`}
+          </script>
           <script
             src="https://cdn.snipcart.com/scripts/2.0/snipcart.js"
             id="snipcart"
