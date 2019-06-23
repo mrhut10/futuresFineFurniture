77258e17497a8fe3ea594c0c91226143a052ead2
mrhut10 <mrhut10@protonmail.com>
2019-06-24 00:19:41 +1000

    extra helper functions

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,6 +6,14 @@ const priceFormat = new Intl.NumberFormat('en-AU', {
   useGrouping: true,
 });
 
+
+const [head, tail] = [
+  a => a.find(() => true),
+  a => a.reverse().find(() => true),
+];
+exports.head = head;
+exports.tail = tail;
+
 const intToPriceFormat = input => priceFormat.format(input / 100);
 
 exports.intToPriceFormat = intToPriceFormat;
