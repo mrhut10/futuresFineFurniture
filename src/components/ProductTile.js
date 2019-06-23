ccc28c6a61d34e89cd7389e37ddd8dbd09b6bea9
mrhut10 <mrhut10@protonmail.com>
2019-06-24 01:53:11 +1000

    refactor

--- src/components/ProductTile.js
+++ src/components/ProductTile.js
@@ -4,13 +4,13 @@ import { intToPriceFormat } from '../helpers/index';
 import { BuyButton } from './snipcart';
 import CategoryTitle from './categoryTile';
 
+const removeDiscount = ({ price, discount }) =>
+  price - (discount && discount > 0 ? discount : 0);
+
 export const ProductTile = ({ name, images, varients, varientLock, slug }) => {
-  const removeDiscount = item =>
-    item.price - (item.discount && item.discount > 0 ? item.discount : 0);
   const minPricedVarient = varients
-    .filter(varient => varient.price && varient.price > 0)
-    .sort((a, b) => removeDiscount(a) - removeDiscount(b))
-    .find(() => true); // cheat way of making a head function
+    .filter(({ price }) => price && price > 0)
+    .sort((a, b) => removeDiscount(a) - removeDiscount(b))[0];
 
   return (
     <CategoryTitle
@@ -21,19 +21,25 @@ export const ProductTile = ({ name, images, varients, varientLock, slug }) => {
       slug={slug}
       Children={
         <div className="flex font-semibold items-baseline justify-between mt-auto mx-auto p-4">
-          <small>
-            from {intToPriceFormat(removeDiscount(minPricedVarient))}
-          </small>
-          <BuyButton
-            name={name}
-            id={name}
-            url="https://www.futuresfinefurnitureandbedding.com/snipcart.json"
-            price={varients[0].price}
-            varients={varients}
-            value={minPricedVarient.varientName}
-          >
-            Add&nbsp;to&nbsp;Cart
-          </BuyButton>
+          {minPricedVarient ? (
+            <>
+              <small>
+                from {intToPriceFormat(removeDiscount(minPricedVarient))}
+              </small>
+              <BuyButton
+                name={name}
+                id={name}
+                url="https://www.futuresfinefurnitureandbedding.com/snipcart.json"
+                price={removeDiscount(varients[0])}
+                varients={varients}
+                value={minPricedVarient.varientName}
+              >
+                Add&nbsp;to&nbsp;Cart
+              </BuyButton>
+            </>
+          ) : (
+            <>see details</>
+          )}
         </div>
       }
     />
