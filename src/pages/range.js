75276d4e32a11cbd450efd4486872dc674c00a5d
Luke Bennett <hello@lukebennett.com.au>
2019-06-29 13:31:06 +1000

    Style /range page

--- src/pages/range.js
+++ src/pages/range.js
@@ -7,6 +7,10 @@ import CategoriesAll from '../components/CategoriesAll';
 const rangePage = () => (
   <Layout>
     <SEO title="Product Range" />
+    <div className="max-w-4xl mx-auto p-4">
+      <h1 className="font-bold mb-4 text-2xl text-maroon-600">
+        Pick a Category
+      </h1>
       <CategoriesAll />
     </div>
   </Layout>
