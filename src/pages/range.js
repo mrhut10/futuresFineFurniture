3336f6cf4ae3ce306b7b190dcb30d03c8635d92a
Luke Bennett <hello@lukebennett.com.au>
2019-06-30 19:51:36 +1000

    Add vertical padding to /range page

--- src/pages/range.js
+++ src/pages/range.js
@@ -7,7 +7,7 @@ import CategoriesAll from '../components/CategoriesAll';
 const rangePage = () => (
   <Layout>
     <SEO title="Product Range" />
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-4xl mx-auto px-4 py-12">
       <h1 className="font-bold mb-4 text-2xl text-maroon-600">
         Pick a Category
       </h1>
