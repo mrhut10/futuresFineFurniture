8dc1c7b04524f5de6805f054a85623ebcfa10a55
mrhut10 <mrhut10@protonmail.com>
2019-07-01 05:27:47 +1000

    new collections page draft

---
+++ src/pages/collections.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import * as R from 'ramda';
+import { StaticQuery, graphql, Link } from 'gatsby';
+import slugify from 'slugify';
+import Layout from '../components/layout';
+import { BulkProducts } from '../components/BulkProducts';
+import Wrapper from '../components/wrapper';
+import { stringify } from 'querystring';
+
+const query = graphql`
+  {
+    allMarkdownRemark(filter: { fields: { type: { eq: "product" } } }) {
+      edges {
+        node {
+          fields {
+            slug
+            range
+            category
+          }
+          frontmatter {
+            title
+            images
+            enabled
+            Category
+            range
+            variants {
+              varientName
+              price
+              discount
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const ProductsByRange = R.groupBy(R.prop('range'));
+
+const queryToProducts = R.compose(
+  R.map(
+    R.compose(
+      node => {
+        const output = {
+          name: R.path(['frontmatter', 'title'])(node),
+          images: R.path(['frontmatter', 'images'])(node),
+          slug: R.path(['fields', 'slug'])(node),
+          variants: R.path(['frontmatter', 'variants'])(node),
+          range: R.path(['fields', 'range'])(node),
+          category: R.path(['fields', 'category'])(node),
+        };
+        return output;
+      },
+      R.prop('node')
+    )
+  ),
+  R.path(['allMarkdownRemark', 'edges'])
+);
+
+const CollectionPage = () => (
+  <Layout>
+    <Wrapper>
+      <div className="pt-8">
+        <div className="px-4">
+          <h1>Explore Our Collections</h1>
+          <StaticQuery
+            query={query}
+            render={R.compose(
+              R.converge(
+                (keys, obj) => (
+                  <ul>
+                    {keys
+                      .sort((ka, kb) => obj[kb].length - obj[ka].length)
+                      .map(key => (
+                        <BulkProducts
+                          key={key}
+                          products={obj[key]}
+                          maxLimit={3}
+                          heading={`${key} ${obj[key].length} items`}
+                        />
+                      ))}
+                  </ul>
+                ),
+                [R.keys, R.map(range => range)]
+              ),
+              ProductsByRange,
+              queryToProducts
+            )}
+          />
+        </div>
+      </div>
+    </Wrapper>
+  </Layout>
+);
+
+export default CollectionPage;
