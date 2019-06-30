8818e2fc4987560cb4dfeb1cebb25a580622f4fe
Luke Bennett <hello@lukebennett.com.au>
2019-06-30 19:51:09 +1000

    Remove unused <div>

--- src/templates/category-route.js
+++ src/templates/category-route.js
@@ -27,33 +27,31 @@ const categoryRoute = ({ data, pageContext }) => {
         keywords={post.frontmatter.keywords || []}
       />
       <Wrapper>
-        <div>
-          <h1 className="font-bold mb-4 text-2xl text-maroon-600">
-            {post.frontmatter.title}
-          </h1>
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
-          {products ? (
-            <BulkProducts
-              products={products.edges
-                .map(({ node }) => ({
-                  name: node.frontmatter.title,
-                  images: R.pathOr('', ['frontmatter', 'images'])(node),
-                  slug: `${pageContext.slug}/${node.fields.productName}`,
-                  variants: R.pathOr([], ['frontmatter', 'variants'])(node),
-                  minPriceCents: R.compose(
-                    R.prop('price'),
-                    minPricedVariant,
-                    R.pathOr([], ['frontmatter', 'variants'])
-                  )(node),
-                  range: R.pathOr([], ['frontmatter', 'range'])(node),
-                }))
-                .sort((a, b) => a.minPriceCents - b.minPriceCents)
-                .sort((a, b) => String(a.range).localeCompare(String(b.range)))}
-            />
-          ) : (
-            <ComingSoon />
-          )}
-        </div>
+        <h1 className="font-bold mb-4 text-2xl text-maroon-600">
+          {post.frontmatter.title}
+        </h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        {products ? (
+          <BulkProducts
+            products={products.edges
+              .map(({ node }) => ({
+                name: node.frontmatter.title,
+                images: R.pathOr('', ['frontmatter', 'images'])(node),
+                slug: `${pageContext.slug}/${node.fields.productName}`,
+                variants: R.pathOr([], ['frontmatter', 'variants'])(node),
+                minPriceCents: R.compose(
+                  R.prop('price'),
+                  minPricedVariant,
+                  R.pathOr([], ['frontmatter', 'variants'])
+                )(node),
+                range: R.pathOr([], ['frontmatter', 'range'])(node),
+              }))
+              .sort((a, b) => a.minPriceCents - b.minPriceCents)
+              .sort((a, b) => String(a.range).localeCompare(String(b.range)))}
+          />
+        ) : (
+          <ComingSoon />
+        )}
       </Wrapper>
     </Layout>
   );
