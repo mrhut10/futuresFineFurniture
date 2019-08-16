9226f66936bac3f605feb368708708c2752ebf65
mrhut10 <mrhut10@protonmail.com>
2019-08-16 14:35:30 +1000

    snipcart.json generator rewrite and fix bug

--- webpage/src/snipcartJSON_maker.js
+++ webpage/src/snipcartJSON_maker.js
@@ -2,55 +2,56 @@ const R = require('ramda');
 
 const positive = input => `${input >= 0 ? '+' : ''}${input}`;
 
-exports.snipcartJson = R.compose(
-  R.map(
-    R.compose(
-      input => ({
-        name: R.prop('title')(input),
-        id: R.prop('title')(input),
-        price: {
-          AUD: R.compose(
-            R.divide(R.__, 100),
-            item => item.price - (item.discount || 0),
-            R.path(['variants', 0])
-          )(input),
-        },
-        url: '/snipcart.json',
-        customFields: R.compose(
-          R.ifElse(
-            R.compose(
-              R.gt(R.__, 1),
-              R.length
-            ),
-            allvariants => [
-              {
-                name: 'Option',
-                options: R.compose(
-                  R.join('|'),
-                  R.map(
-                    vari =>
-                      `${vari.variantName}[${positive(
-                        (vari.price -
-                          (vari.discount || 0) -
-                          R.pathOr(0, ['0', 'price'])(allvariants)) /
-                          100
-                      )}]`
-                  ),
-                  R.filter(item => !item.disabled),
-                  R.filter(item => item.price && item.price > 0)
-                )(allvariants),
-                // allvariants.map().join('|'),
-                type: 'dropdown',
-              },
-            ],
-            R.always([])
-          ),
-          R.prop('variants')
-        )(input),
-      }),
-      R.path(['node', 'frontmatter'])
-    )
-  ),
-  R.filter(R.complement(R.pathOr(false, ['node', 'frontmatter', 'disabled']))),
-  R.path(['data', 'allMarkdownRemark', 'edges'])
-);
+exports.snipcartJson = ({ data }) => {
+  const getEdges = R.path(['allMarkdownRemark', 'edges']);
+  const filterVariants = variants =>
+    variants
+      ? variants
+          .filter(vari => vari.price - vari.discount || 0) // priceless
+          .filter(vari => vari.disabled !== true)
+      : variants;
+
+  const edgeToProduct = ({ node }) => ({
+    name: node.frontmatter.title,
+    disabled:
+      node.frontmatter.disabled === true ||
+      !filterVariants(node.frontmatter.variants) ||
+      filterVariants(node.frontmatter.variants).length === 0,
+    variants: filterVariants(node.frontmatter.variants) || [],
+  });
+
+  const productToSnipcartProductDefinition = ({ name, variants }) => ({
+    name,
+    id: name,
+    price: {
+      AUD:
+        variants.length > 1
+          ? variants[0].price / 100
+          : (variants[0].price - (variants[0].discount || 0)) / 100,
+    },
+    URL: '/snipcart.json',
+    customFields:
+      variants.length > 1
+        ? [
+            {
+              name: 'Option',
+              options: variants
+                .map(
+                  ({ variantName, discount, price }, i, parentArray) =>
+                    `${variantName || 'default'}[${positive(
+                      (price - (discount || 0) - parentArray[0].price) / 100
+                    )}]`
+                )
+                .join('|'),
+              type: 'dropdown',
+            },
+          ]
+        : [],
+  });
+  console.log(getEdges(data).map(productToSnipcartProductDefinition));
+
+  return getEdges(data)
+    .map(edgeToProduct)
+    .filter(product => product.disabled !== true)
+    .map(productToSnipcartProductDefinition);
+};
