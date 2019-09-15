519d83376fec483f97705799be4117d9659aaeae
mrhut10 <mrhut10@protonmail.com>
2019-09-16 04:13:24 +1000

    removed unneeded fields

--- adminpage/schemas/documentCommon.js
+++ adminpage/schemas/documentCommon.js
@@ -4,40 +4,9 @@ export default {
   type: 'object',
   fields: [
     {
-      title: 'Date Created',
-      name: 'dateCreated',
-      type: 'date',
-      options: {
-        dateFormat: 'YYYY-MM-DD',
-        calendarTodayLabel: 'Today'
-      },
-    },
-    {
-      title: 'Date Published',
-      name: 'datePublished',
-      type: 'date',
-      options: {
-        dateFormat: 'YYYY-MM-DD',
-        calendarTodayLabel: 'Today'
-      },
-    },
-    {
-      title: 'Date Updated',
-      name: 'dateUpdated',
-      type: 'date',
-      options: {
-        dateFormat: 'YYYY-MM-DD',
-        calendarTodayLabel: 'Today'
-      },
-    },
-    {
-      title: 'Date Disabled',
-      name: 'dateDisabled',
-      type: 'date',
-      options: {
-        dateFormat: 'YYYY-MM-DD',
-        calendarTodayLabel: 'Today'
-      },
+      title: 'Disable',
+      name: 'disable',
+      type: 'boolean',
     }
   ]
 };
