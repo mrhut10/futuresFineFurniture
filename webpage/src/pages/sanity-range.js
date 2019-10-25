88252bf6edbb4a2c47ec479b9d922dd3a413155d
mrhut10 <mrhut10@protonmail.com>
2019-10-25 11:27:58 +1100

    sanity range page

---
+++ webpage/src/pages/sanity-range.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+import CategoryTitle from '../components/CategoryTile';
+
+const sanityRange = () => (
+  <Layout>
+    <SEO title="Product Range" />
+    <div className="max-w-4xl mx-auto px-4 py-12">
+      <h1 className="font-bold mb-4 text-2xl text-maroon-600">
+        Pick a Category
+      </h1>
+      <div className="flex flex-wrap justify-center mx-auto w-full">
+        <div className="flex flex-wrap -m-2">
+          <StaticQuery
+            query={query}
+            render={queryData => {
+              //return JSON.stringify(queryData);
+              return queryData.allSanityCategory.edges.map(({ node }) => (
+                <CategoryTitle
+                  key={node.name}
+                  name={node.name}
+                  slug={node.slug? `/sanity/category/${node.slug.current}`.toLowerCase() : ''}
+                  hoverText={node.description}
+                  images={node.cover ? [node.cover.asset.fluid] : null}
+                  comingSoon={false}
+                  height={300}
+                />
+              ));
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  </Layout>
+);
+
+export default sanityRange;
+const query = graphql`
+{
+  allSanityCategory(filter: { parent: { id: { eq: null } } common: {disable: {ne: true}}}) {
+    edges {
+      node {
+        name
+        slug {
+          current
+        }
+        cover {
+          asset {
+            fluid(maxWidth: 200) {
+              src
+            }
+          }
+        }
+        description
+        keywords
+        common {
+          disable
+        }
+      }
+    }
+  }
+}
+`;
