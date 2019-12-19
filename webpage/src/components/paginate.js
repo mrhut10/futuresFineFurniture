d3c947e90f7fc0be75d5353398f4985dc0d2743e
mrhut10 <mrhut10@protonmail.com>
2019-12-20 10:54:15 +1100

    new pageinate

--- webpage/src/components/paginate.js
+++ webpage/src/components/paginate.js
@@ -1,36 +1,14 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import queryStrings from 'query-string';
-import { changeObjectProb } from "../helpers";
 
-const Paginate = ({currentNumber, maxNumber, location, productsPerPage}) => {
-  const LinkPrev =
-    currentNumber <= 1 ? null : (
-      <Link
-        to={location.pathname}
-        state={{
-          pageNum: currentNumber - 1,
-          productsPerPage,
-        }}
-      >
-        Prev {"<<"}
-      </Link>
-    );
-  const LinkNext =
-    currentNumber >= maxNumber ? null : (
-      <Link
-        to={location.pathname}
-        state={{
-          pageNum: currentNumber + 1,
-          productsPerPage,
-        }}
-      >
-        Next {">>"}
-      </Link>
-    );
+const Paginate = ({pageNum, pageTotal, Back, Forward}) => {
+  
   return (
     <p>
-      {LinkPrev} page: {currentNumber} of {maxNumber} {LinkNext}
+      Page {pageNum} of {pageTotal} <br/>
+      {pageNum > 1 ? (
+        <>{Back}<span className="m-2" /></> 
+      ) : null}
+      {pageNum < pageTotal ? Forward : null}
     </p>
   );
 };
