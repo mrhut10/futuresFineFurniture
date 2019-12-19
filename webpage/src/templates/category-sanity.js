ce742350b88acef3bac21b0305fc6f6ae08cfbc8
mrhut10 <mrhut10@protonmail.com>
2019-12-20 10:28:59 +1100

    catigory page scafold

--- webpage/src/templates/category-sanity.js
+++ webpage/src/templates/category-sanity.js
@@ -15,70 +15,32 @@ import {
 } from '../components/products';
 import NotAvaliable from '../components/NotAvaliable';
 import Paginate from '../components/paginate';
+import { product } from 'ramda';
 
 const categoryRoute = ({ data, pageContext, location }) => {
-  const { productsPerPage, pageNum } = location.state || {productsPerPage: 30, pageNum: 1};
-  const { disable } = data.sanityCategory.common || { disable: false };
-  const totalProducts = Products({
-    filters:[
-      CommonFilters.hideDisable,
-      node => node.category._id === pageContext.catigoryID,
-    ],
-    perPage: 1000000,
-    pageNum: 1,
-  }).length;
-  const products = Products({
-    filters: [
-      CommonFilters.hideDisable,
-      node => node.category._id === pageContext.catigoryID,
-    ],
-    perPage: productsPerPage,
+  const {
+    productsPerPage,
     pageNum,
-    sorters: [
-      (a, b) => {
-        const findCheapVar = node =>
-          node.variants.reduce((ac, bc) => (bc.price < ac.price ? bc : ac));
-        return findCheapVar(a).price - findCheapVar(b).price;
-      },
-      (a, b) => {
-        let output = 0;
-        if (a.range && !b.range) {
-          output = a.range.id < b.range.id ? 1 : -1;
-        } else if (a.range || b.range) {
-          output = 1;
-        }
-        return output;
-      },
-    ],
-  });
+    totalPages,
+    totalProducts
+  } = pageContext;
+  const { name, keywords } = data.sanityCategory;
+
+  const { disable } = data.sanityCategory.common || { disable: false };
   return (
     <Layout>
-      <SEO
-        title={data.sanityCategory.name}
-        keywords={data.sanityCategory.keywords || []}
-      />
+      <SEO title={name} keywords={keywords || []} />
       <Wrapper>
-        <h1 className="font-bold mb-4 text-2xl text-maroon-600">
-          {data.sanityCategory.name}
-        </h1>
+        <h1 className="font-bold mb-4 text-2xl text-maroon-600">{name}</h1>
         <p>
-          Products per Page:
-          <ProductsPerPage
-            defaultVal={10}
-            options={[30, 60, 120]}
-            location={location}
-            value={productsPerPage}
-          />
+          Total {name} products found: {totalProducts} <br />
+          Page {pageNum} of {Math.ceil(totalProducts / productsPerPage)} <br />
+          {pageNum > 1 ? <Link>Previous Page</Link> : null}
+          {pageNum < Math.ceil(totalProducts / productsPerPage) ? (
+            <Link>Next Page</Link>
+          ) : null}
         </p>
-        <Paginate
-          currentNumber={pageNum}
-          maxNumber={Math.ceil(totalProducts / productsPerPage)}
-          location={location}
-          productsPerPage={productsPerPage}
-        />
-        {products && !disable ? (
-          <ProductGroupRender products={products} />
-        ) : (
+        {!disable ? 'hi' : (
           <NotAvaliable
             text={disable ? 'Not Avaliable' : 'Coming Soon'}
             subtext={
@@ -89,21 +51,6 @@ const categoryRoute = ({ data, pageContext, location }) => {
             showContact
           />
         )}
-        <p>
-          Products per Page:
-          <ProductsPerPage
-            defaultVal={10}
-            options={[30, 60, 120]}
-            location={location}
-            value={productsPerPage}
-          />
-        </p>
-        <Paginate
-          currentNumber={pageNum}
-          maxNumber={Math.ceil(totalProducts / productsPerPage)}
-          location={location}
-          productsPerPage={productsPerPage}
-        />
       </Wrapper>
     </Layout>
   );
@@ -111,11 +58,11 @@ const categoryRoute = ({ data, pageContext, location }) => {
 
 export const query = graphql`
   query sanity_categoryPageQuery(
-    $catigoryID: String
+    $categoryID: String
     $skip: Int!
     $productsPerPage: Int!
   ) {
-    sanityCategory(_id: { eq: $catigoryID }) {
+    sanityCategory(_id: { eq: $categoryID }) {
       _id
       name
       slug {
