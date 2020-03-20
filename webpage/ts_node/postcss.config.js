9e26bd4000de40946cbe5d1348be1d7b3c0c10f3
mrhut10 <mrhut10@protonmail.com>
2020-03-20 16:05:36 +1100

    removed old version

--- webpage/ts_node/postcss.config.js
+++
@@ -1,7 +0,0 @@
-const autoprefixer = require('autoprefixer');
-const tailwindcss = require('tailwindcss');
-const postcssImport = require('postcss-import');
-
-module.exports = {
-  plugins: [postcssImport, tailwindcss, autoprefixer],
-};
